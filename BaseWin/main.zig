ac9d4f9bb5ef31a6334fcd7d4e79cb57b1bc5678
DarknessFX <35434089+DarknessFX@users.noreply.github.com>
2023-10-26 23:44:33 +0100

    Add files via upload

---
+++ BaseWin/main.zig
@@ -0,0 +1,127 @@
+const std = @import("std");
+const win = struct {
+  usingnamespace std.os.windows;
+  usingnamespace std.os.windows.user32;
+  usingnamespace std.os.windows.kernel32;
+};
+const WINAPI = win.WINAPI;
+const L = std.unicode.utf8ToUtf16LeStringLiteral;
+
+pub export fn WinMain(hInstance: win.HINSTANCE, hPrevInstance: ?win.HINSTANCE, 
+  pCmdLine: ?win.LPWSTR, nCmdShow: win.INT) callconv(WINAPI) win.INT {
+  _ = hPrevInstance;
+  _ = pCmdLine;
+
+  const class_name = L("BaseWin");
+  const wnd_class: win.WNDCLASSEXW = .{
+    .cbSize = @sizeOf(win.WNDCLASSEXW),
+    .style = win.CS_CLASSDC | win.CS_HREDRAW | win.CS_VREDRAW,
+    .lpfnWndProc = WindowProc,
+    .cbClsExtra = 0, 
+    .cbWndExtra = 0,
+    .hInstance = hInstance,
+    .hIcon = null, 
+    .hCursor = null, 
+    .hbrBackground = null, 
+    .lpszMenuName = null,
+    .lpszClassName = class_name,
+    .hIconSm = null,
+  };
+
+  _ = win.RegisterClassExW(&wnd_class);
+  defer _ = win.UnregisterClassW(class_name, hInstance);
+
+  const hWnd: win.HWND = win.CreateWindowExW(
+    0, class_name, L("BaseWin"), win.WS_OVERLAPPEDWINDOW,
+    win.CW_USEDEFAULT, win.CW_USEDEFAULT, win.CW_USEDEFAULT, win.CW_USEDEFAULT, 
+    null, null, hInstance, null) orelse undefined;
+
+  if (hWnd == undefined) {
+    const err = win.GetLastError();
+    std.log.err("{}", .{ err });
+    return @intFromEnum(err);
+  }
+  defer _ = win.DestroyWindow(hWnd);
+
+  _ = win.ShowWindow(hWnd, nCmdShow);
+  _ = win.updateWindow(hWnd) catch undefined;
+
+  var msg: win.MSG = std.mem.zeroes(win.MSG);
+  while (win.GetMessageW(&msg, null, 0, 0) != 0) {
+    _ = win.TranslateMessage(&msg);
+    _ = win.DispatchMessageW(&msg);
+  }
+
+  return 0;
+}
+
+
+fn WindowProc( hWnd: win.HWND, uMsg: win.UINT, wParam: win.WPARAM, lParam: win.LPARAM ) callconv(WINAPI) win.LRESULT {
+  switch (uMsg) {
+    win.WM_DESTROY => {
+      win.PostQuitMessage(0);
+      return 0;
+    },
+    win.WM_PAINT => {
+      var ps: PAINTSTRUCT = undefined;
+      const mens: win.LPCWSTR = L("Hello world!");
+      const mens_len: win.INT = @as(win.INT, @intCast(std.mem.len(mens)));
+      const hdc: HDC = BeginPaint(hWnd, &ps) orelse undefined;
+      _ = FillRect(hdc, &ps.rcPaint, @ptrFromInt(COLOR_WINDOW+1));
+      _ = TextOutW(hdc, 20, 20, mens, mens_len);
+      _ = EndPaint(hWnd, &ps);
+    },
+    else => _=.{},
+  }
+
+  return win.DefWindowProcW(hWnd, uMsg, wParam, lParam);
+}
+
+
+// Fix for libc linking error.
+pub export fn wWinMain(hInstance: win.HINSTANCE, hPrevInstance: ?win.HINSTANCE, 
+  pCmdLine: ?win.LPWSTR, nCmdShow: win.INT) callconv(WINAPI) win.INT {
+  return WinMain(hInstance, hPrevInstance, pCmdLine, nCmdShow);
+}
+
+const COLOR_WINDOW = 5;
+pub const HDC = *opaque{};
+pub const HBRUSH = *opaque{};
+pub const PAINTSTRUCT = extern struct {
+  hdc: HDC,
+  fErase: win.BOOL,
+  rcPaint: win.RECT,
+  fRestore: win.BOOL,
+  fIncUpdate: win.BOOL,
+  rgbReserved: [32]win.BYTE
+};
+
+pub extern "user32" fn BeginPaint(
+  hWnd: ?win.HWND,
+  lpPaint: ?*PAINTSTRUCT,
+) callconv(WINAPI) ?HDC;
+
+pub extern "user32" fn FillRect(
+  hDC: ?HDC,
+  lprc: ?*const win.RECT,
+  hbr: ?HBRUSH
+) callconv(WINAPI) win.INT;
+
+pub extern "user32" fn EndPaint(
+  hWnd: win.HWND,
+  lpPaint: *const PAINTSTRUCT
+) callconv(WINAPI) win.BOOL;
+
+pub extern "gdi32" fn TextOutW(
+  hDC: ?HDC,
+  x: win.INT,
+  y: win.INT,
+  lpString: win.LPCWSTR,
+  c: win.INT
+) callconv(WINAPI) win.BOOL;
+
+//   _ = win.MessageBoxA(null, "Sample text.", "Title", win.MB_OK);
+//  _ = OutputDebugStringA("\x1b[31mRed\x1b[0m");
+pub extern "kernel32" fn OutputDebugStringA(
+  lpOutputString: win.LPCSTR
+) callconv(WINAPI) win.INT;
