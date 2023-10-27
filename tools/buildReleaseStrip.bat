c851943c69d06a974952714480c2a7eb5cffcec6
DarknessFX <35434089+DarknessFX@users.noreply.github.com>
2023-10-27 09:07:25 +0100

    Add files via upload

--- tools/buildReleaseStrip.bat
+++ tools/buildReleaseStrip.bat
@@ -11,7 +11,7 @@ REM  Adding cImport .H include folders (note: -I SPACE Folder path):
 REM    -I %CD%\lib\microui -I %CD%\lib\SDL2\include
 REM
 REM Full extra_args sample of a project that use SDL2 + OpenGL + microui :
-REM  SET extra_args=-lSDL2 -lOpenGL32 -L %CD%\lib\SDL2 -I %CD%\lib\microui -I %CD%\lib\SDL2\include
+REM  SET extra_args=-lSDL2 -lOpenGL32 -L "%CD%\lib\SDL2" -I "%CD%\lib\microui" -I "%CD%\lib\SDL2\include"
 
 SET extra_args=
 
@@ -20,7 +20,7 @@ REM AddCSource
 REM ==========
 REM If your project use C Source Files, add here the list of files you want to add to your build.
 REM 
-REM SET addCSourceFile=%CD%\lib\microui\microui.c
+REM SET addCSourceFile="%CD%\lib\microui\microui.c"
 
 SET addCSourceFile=
 
@@ -47,7 +47,7 @@ FINDSTR /L linkLibC build.zig > NUL && (
 
 REM OUTPUT TO ZIG_REPORT.EXE
 > bin/ReleaseStrip/obj/zig_report.txt (
-  zig build-exe -O ReleaseSmall %rcmd% %libc% %addCSourceFile% -fstrip -fsingle-threaded --color on -femit-bin=bin/ReleaseStrip/%ProjectName%.exe -femit-asm=bin/ReleaseStrip/obj/%ProjectName%.s -femit-llvm-ir=bin/ReleaseStrip/obj/%ProjectName%.ll -femit-llvm-bc=bin/ReleaseStrip/obj/%ProjectName%.bc -femit-h=bin/ReleaseStrip/obj/%ProjectName%.h -ftime-report -fstack-report %extra_args% --name %ProjectName% main.zig
+  zig build-exe -O ReleaseSmall %rcmd% %libc% -fstrip -fsingle-threaded --color off -femit-bin=bin/ReleaseStrip/%ProjectName%.exe -femit-asm=bin/ReleaseStrip/obj/%ProjectName%.s -femit-llvm-ir=bin/ReleaseStrip/obj/%ProjectName%.ll -femit-llvm-bc=bin/ReleaseStrip/obj/%ProjectName%.bc -femit-h=bin/ReleaseStrip/obj/%ProjectName%.h -ftime-report -fstack-report %extra_args% --name %ProjectName% main.zig %addCSourceFile%
 ) 2>&1 
 MOVE %CD%\bin\ReleaseStrip\%ProjectName%.exe.obj %CD%\bin\ReleaseStrip\obj > NUL
 
