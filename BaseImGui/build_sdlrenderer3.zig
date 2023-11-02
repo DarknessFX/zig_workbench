7920f39e2072d645878ae593c8dc6801580569b9
DarknessFX <darkness@fatorx.com.br>
2023-11-02 07:13:28 +0000

    .

---
+++ BaseImGui/build_sdlrenderer3.zig
@@ -0,0 +1,78 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+  //Build
+  const target = b.standardTargetOptions(.{});
+  const optimize = b.standardOptimizeOption(.{});
+
+  const projectname = "BaseImGui";
+  const rootfile = "main.zig";
+
+  const exe = b.addExecutable(.{
+    .name = projectname,
+    .root_source_file = .{ .path = rootfile },
+    .target = target,
+    .optimize = optimize,
+  });
+  exe.addWin32ResourceFile(.{
+    .file = .{ .path = projectname ++ ".rc" },
+    .flags = &.{"/c65001"}, // UTF-8 codepage
+  });
+
+  exe.linkSystemLibrary("gdi32");
+  exe.linkSystemLibrary("dwmapi");
+  exe.linkSystemLibrary("SDL3");
+
+  exe.addIncludePath( .{ .path = "lib/imgui" }  );
+  exe.addIncludePath( .{ .path = "lib/SDL3/include" }  );
+
+  var c_srcs = .{
+    "lib/imgui/cimgui.cpp",
+    "lib/imgui/cimgui_impl_sdl3.cpp",
+    "lib/imgui/cimgui_impl_sdlrenderer3.cpp",
+    "lib/imgui/imgui.cpp",
+    "lib/imgui/imgui_widgets.cpp",
+    "lib/imgui/imgui_draw.cpp",
+    "lib/imgui/imgui_tables.cpp",
+    "lib/imgui/imgui_demo.cpp",
+    "lib/imgui/imgui_impl_sdl3.cpp",
+    "lib/imgui/imgui_impl_sdlrenderer3.cpp"
+  };
+  inline for (c_srcs) |c_cpp| {
+    exe.addCSourceFile(.{
+      .file = std.build.LazyPath.relative(c_cpp), 
+      .flags = &.{ }
+    });
+  }
+  
+  b.installBinFile("lib/SDL3/SDL3.dll", "SDL3.dll");
+  exe.linkLibCpp();
+
+  switch (optimize) {
+    .Debug =>  b.exe_dir = "bin/Debug",
+    .ReleaseSafe =>  b.exe_dir = "bin/ReleaseSafe",
+    .ReleaseFast =>  b.exe_dir = "bin/ReleaseFast",
+    .ReleaseSmall =>  b.exe_dir = "bin/ReleaseSmall",
+    //else  =>  b.exe_dir = "bin/Else",
+  }
+  b.installArtifact(exe);
+
+  //Run
+  const run_cmd = b.addRunArtifact(exe);
+  run_cmd.step.dependOn(b.getInstallStep());
+  if (b.args) |args| {
+    run_cmd.addArgs(args);
+  }
+  const run_step = b.step("run", "Run the app");
+  run_step.dependOn(&run_cmd.step);
+
+  //Tests
+  const unit_tests = b.addTest(.{
+    .root_source_file = .{ .path = rootfile },
+    .target = target,
+   .optimize = optimize,
+  });
+  const run_unit_tests = b.addRunArtifact(unit_tests);
+  const test_step = b.step("test", "Run unit tests");
+  test_step.dependOn(&run_unit_tests.step);
+}
---
+++ BaseImGui/main_sdlrenderer3.zig
@@ -0,0 +1,153 @@
+const std = @import("std");
+const win = struct {
+  usingnamespace std.os.windows;
+  usingnamespace std.os.windows.user32;
+  usingnamespace std.os.windows.kernel32;
+  usingnamespace std.os.windows.gdi32;
+};
+const WINAPI = win.WINAPI;
+const L = std.unicode.utf8ToUtf16LeStringLiteral;
+
+// NOTE ABOUT VSCODE + ZLS:
+// Use full path for all cIncludes:
+//   @cInclude("C:/zig_microui/lib/SDL2/include/SDL.h"); 
+const im = @cImport({
+  @cInclude("lib/imgui/cimgui.h");
+  @cInclude("lib/imgui/cimgui_impl_sdl3.h");
+  @cInclude("lib/imgui/cimgui_impl_sdlrenderer3.h");
+});
+
+const sdl = @cImport({
+  @cInclude("lib/SDL3/include/SDL.h");
+  @cInclude("lib/SDL3/include/SDL_opengl.h");
+});
+
+const ImVec4 = struct {
+  x: f32,
+  y: f32,
+  z: f32,
+  w: f32
+};
+
+pub fn main() void {
+  HideConsole();
+
+  _ = sdl.SDL_Init(sdl.SDL_INIT_VIDEO | sdl.SDL_INIT_TIMER | sdl.SDL_INIT_GAMEPAD);
+  _ = sdl.SDL_SetHint(sdl.SDL_HINT_IME_SHOW_UI, "1");
+
+  var window_flags: sdl.SDL_WindowFlags = sdl.SDL_WINDOW_RESIZABLE | sdl.SDL_WINDOW_HIGH_PIXEL_DENSITY | sdl.SDL_WINDOW_HIDDEN;
+  var window: *sdl.SDL_Window = sdl.SDL_CreateWindow("Dear ImGui SDL3+SDL_Renderer example", 1280, 720, window_flags).?;
+  var renderer: *sdl.SDL_Renderer = sdl.SDL_CreateRenderer(window, null, sdl.SDL_RENDERER_PRESENTVSYNC | sdl.SDL_RENDERER_ACCELERATED).?;
+  _ = sdl.SDL_SetWindowPosition(window, sdl.SDL_WINDOWPOS_CENTERED, sdl.SDL_WINDOWPOS_CENTERED);
+  _ = sdl.SDL_ShowWindow(window);
+
+  _ = im.ImGui_CreateContext(null);
+  var io: *im.struct_ImGuiIO_t = im.ImGui_GetIO();
+  io.ConfigFlags |= im.ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+  io.ConfigFlags |= im.ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+  io.ConfigFlags |= im.ImGuiConfigFlags_DockingEnable;       // Enable Docking
+
+  im.ImGui_StyleColorsDark(null);
+
+  var cwindow = @as(?*im.SDL_Window , @ptrCast(window));
+  var crenderer = @as(?*im.SDL_Renderer , @ptrCast(renderer));
+  _ = im.cImGui_ImplSDL3_InitForSDLRenderer(cwindow, crenderer);
+  _ = im.cImGui_ImplSDLRenderer3_Init(crenderer);
+
+  var show_demo_window: bool = true;
+  var show_another_window: bool = false;
+  var clear_color: ImVec4 = ImVec4{.x=0.45, .y=0.55, .z=0.60, .w=1.00};
+  var f: f32 = 0.0;
+  var counter: u16 = 0;
+
+  var event: sdl.SDL_Event = undefined;
+  var cevent = @as([*c]const im.SDL_Event , @ptrCast(&event));
+  var done: bool = false;
+  while (!done) {
+    while (sdl.SDL_PollEvent(&event) == 1) {
+      _ = im.cImGui_ImplSDL3_ProcessEvent(cevent);
+      if (event.type == sdl.SDL_EVENT_QUIT) {
+        done = true; }
+      if (event.type == sdl.SDL_EVENT_WINDOW_CLOSE_REQUESTED  and 
+        event.window.windowID == sdl.SDL_GetWindowID(window)) {
+        done = true; }
+    }
+
+    im.cImGui_ImplSDLRenderer3_NewFrame();
+    im.cImGui_ImplSDL3_NewFrame();
+    im.ImGui_NewFrame();
+
+    _ = im.ImGui_DockSpaceOverViewport();
+    
+    if (show_demo_window)
+      im.ImGui_ShowDemoWindow(&show_demo_window);
+
+    {
+      _ = im.ImGui_Begin("Hello, world!", null, im.ImGuiWindowFlags_NoSavedSettings);
+      im.ImGui_Text("This is some useful text.");
+      _ = im.ImGui_Checkbox("Demo Window", &show_demo_window);
+      _ = im.ImGui_Checkbox("Another Window", &show_another_window);
+
+      _ = im.ImGui_SliderFloat("float", &f, 0.0, 1.0);
+      _ = im.ImGui_ColorEdit3("clear color", @as([*c]f32,  &clear_color.x), 0);
+
+      if (im.ImGui_Button("Button"))
+          counter += 1;
+      im.ImGui_SameLine();
+      im.ImGui_Text("counter = %d", counter);
+
+      im.ImGui_Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0 / io.Framerate, io.Framerate);
+      im.ImGui_End();
+    }
+
+    if (show_another_window) {
+      _ = im.ImGui_Begin("Hello, world!", &show_another_window, im.ImGuiWindowFlags_NoSavedSettings);
+      im.ImGui_Text("Hello from another window");
+      if (im.ImGui_Button("Close Me"))
+        show_another_window = false;
+      im.ImGui_End();
+    }
+
+    im.ImGui_Render();
+    _ = sdl.SDL_SetRenderDrawColor(renderer, 
+      toU8(clear_color.x * 255), toU8(clear_color.y * 255),
+      toU8(clear_color.z * 255), toU8(clear_color.w * 255));
+    _ = sdl.SDL_RenderClear(renderer);
+    im.cImGui_ImplSDLRenderer3_RenderDrawData(im.ImGui_GetDrawData());
+    _ = sdl.SDL_RenderPresent(renderer);
+  }
+
+  im.cImGui_ImplSDLRenderer3_Shutdown();
+  im.cImGui_ImplSDL3_Shutdown();
+  im.ImGui_DestroyContext(null);
+
+  sdl.SDL_DestroyRenderer(renderer);
+  sdl.SDL_DestroyWindow(window);
+  sdl.SDL_Quit();
+
+  return;
+}
+
+fn toU8(value: f32) u8 {
+  return @as(u8, @intFromFloat(value));
+}
+
+fn HideConsole() void {
+  const BUF_TITLE = 1024;
+  var hwndFound: win.HWND = undefined;
+  var pszWindowTitle: [BUF_TITLE:0]win.CHAR = std.mem.zeroes([BUF_TITLE:0]win.CHAR); 
+
+  _ = GetConsoleTitleA(&pszWindowTitle, BUF_TITLE);
+  hwndFound=FindWindowA(null, &pszWindowTitle);
+  _ = win.ShowWindow(hwndFound, win.SW_HIDE);
+}
+
+pub extern "kernel32" fn GetConsoleTitleA(
+    lpConsoleTitle: win.LPSTR,
+    nSize: win.DWORD,
+) callconv(win.WINAPI) win.DWORD;
+
+pub extern "kernel32" fn FindWindowA(
+    lpClassName: ?win.LPSTR,
+    lpWindowName: ?win.LPSTR,
+) callconv(win.WINAPI) win.HWND;
