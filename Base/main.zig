9eba8f99f7ebd680aee2871620c8c5d947e52fb3
DarknessFX <darkness@fatorx.com.br>
2026-04-25 22:14:55 +0100

    .

--- Base/main.zig
+++ Base/main.zig
@@ -1,47 +1,103 @@
 //!zig-autodoc-section: Base.Main
 //! Base\\main.zig :
 //!   Template for a console program.
-// Build using Zig 0.15.1
+// Build using Zig 0.16.0
 
 //=============================================================================
 //#region MARK: GLOBAL
 //=============================================================================
-
 const std = @import("std");
+const Io = std.Io;
+var appinit: std.process.Init = undefined;
+
+const print = std.debug.print;
+/// Print line directly when text don't need formating.
+inline fn printLine(line: []const u8) void { print("{s}\n", .{ line }); }
+
+const log = std.log.info;
+/// Log line directly when text don't need formating.
+inline fn logLine(line: []const u8) void { log("{s}\n", .{ line }); }
 
 //#endregion ==================================================================
 //#region MARK: MAIN
 //=============================================================================
+pub fn main(init: std.process.Init) !void {
+  appinit = init;
 
-pub fn main() void {
-  printLine("Base Template :");
-  print("All your {s} are belong to us.\n", .{"codebase"});
+  // JuicyMain
+  try checkMemory();
+  try checkStdOut();
+  try printArgs();
+  try iterateArgs();
+  try iterateArgs2();
+  printEnvArgs();
+  searchEnvArgs();
+  printEnvArgsCount();
+
 }
 
 //#endregion ==================================================================
 //#region MARK: UTIL
 //=============================================================================
+fn checkMemory() !void {
+  const ptr = try appinit.gpa.create(i32);
+  defer appinit.gpa.destroy(ptr);
 
-/// Print to standard out.
-fn print(comptime fmt: []const u8, args: anytype) void {
-  // debug.print output to StdErr ( is a shortcut to
-  // std.io.getStdErr() ), changed to a custom warper
-  // to StdOut ignoring any error.
-
-  var stdout_buffer: [1024]u8 = undefined;
-  var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-  const stdout = &stdout_writer.interface;
-  stdout.print(fmt, args) catch unreachable;
-  stdout.flush() catch unreachable;
+  const ptr2 = try appinit.arena.allocator().create(i32);
+  defer appinit.arena.allocator().destroy(ptr2);
 }
 
-/// Print line directly when text don't need formating.
-inline fn printLine(line: []const u8) void { print("{s}\n", .{ line }); }
+fn checkStdOut() !void {
+  try Io.File.stdout().writeStreamingAll(appinit.io, "Base Template: - Hello, world!\n");
+  printLine("(print) Testing the features of Zig's new JuicyMain.");
+  logLine("(log) Testing the features of Zig's new JuicyMain.");
+}
+
+fn printArgs() !void {
+  const args = try appinit.minimal.args.toSlice(appinit.arena.allocator());
+  for (args, 0..) |arg, i| {
+    log("arg[{d}] = {s}", .{ i, arg });
+  }
+}
+
+fn iterateArgs() !void {
+  var argsIt = try appinit.minimal.args.iterateAllocator(appinit.gpa);
+  defer argsIt.deinit();
+  while (argsIt.next()) |arg| {
+    log("arg: {s}", .{arg});
+  }
+}
+
+fn iterateArgs2() !void {
+  var argsIt = try appinit.minimal.args.iterateAllocator(appinit.arena.allocator());
+  defer argsIt.deinit();
+  while (argsIt.next()) |arg| {
+    log("arg: {s}", .{arg});
+  }
+}
+
+fn printEnvArgs() void {
+  for (appinit.environ_map.keys(), appinit.environ_map.values()) |key, value| {
+    log("env: {s}={s}", .{ key, value });
+  }
+}
+
+fn printEnvArgsCount() void {
+  log("{d} env vars", .{appinit.environ_map.count()});
+}
+
+fn searchEnvArgs() void {
+  log("contains HOME: {any}", .{appinit.minimal.environ.contains(appinit.gpa, "HOME")});
+  log("contains HOME (unempty): {any}", .{appinit.minimal.environ.containsUnempty(appinit.gpa, "HOME")});
+  if (appinit.minimal.environ.containsConstant("APPDATA")) {
+    log("contains APPDATA: {any}", .{ true });
+    log("contains APPDATA (value): {?s}", .{ appinit.environ_map.get("APPDATA") });
+  }
+}
 
 //#endregion ==================================================================
 //#region MARK: TEST
 //=============================================================================
-
 test " print" {
   print("\nTest print(): Hello {s}!", .{" world"});
   try std.testing.expect(true);
@@ -49,6 +105,28 @@ test " print" {
 
 test " printLine" {
   printLine("\nTest printLine(): No error is good.");
+  try std.testing.expect(true);
+}
+
+test " log" {
+  log("\nTest log(): Hello {s}!", .{" world"});
+  print("\nTest log(): Hello {s}!", .{" world"});
+  try std.testing.expect(true);
+}
+
+test " logLine" {
+  logLine("\nTest logLine(): No error is good.");
+  printLine("\nTest logLine(): No error is good.");
+  try std.testing.expect(true);
+}
+
+test " checkMemory" {
+  var _arena: std.heap.ArenaAllocator = .init(std.heap.page_allocator);
+  defer _arena.deinit();
+
+  appinit.gpa = std.testing.allocator;
+  appinit.arena = &_arena;
+  try checkMemory();
   try std.testing.expect(true);
 }
 
